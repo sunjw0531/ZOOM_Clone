777f6dd99ac38433dca1bdb03dca73fedc1ebcc8
sunjw0531 <sunjw0531@naver.com>
2022-02-15 18:15:50 +0900

    WebSocket setup
    create 'http' and 'WebSocket' for exposing the server and create ws Server on the http server.
    therefore localhost can handle http and ws request on the same port.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
+
 import express from "express"
+import http from "http"
+import WebSocket from "ws"
 
 const app = express();
 
-app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
+// Allow pug's script work
+app.use("/public", express.static(__dirname + "/public"));
+//
+app.get("/", (req,res) => res.render("home"));
+app.get("/*", (req,res) => res.redirect("/"));
 
-app.get("/", (req, res) =>{
-    res.render("home");
-})
+const handleListen = () => console.log(`listening on http://localhost:3000`);
 
-const listen = () => console.log("Listening on http://localhost:3000");
+const server = http.createServer(app);
+const ws = new WebSocket.Server({server});
 
-app.listen(3000, listen);
+server.listen(3000, handleListen);
